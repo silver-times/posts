b5dd1800958798b02461b9842f18ae40bd884541
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-11-21 22:52:33 +0100

    add: seed db

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -42,16 +42,19 @@ export const Home = () => {
   return (
     <div className="flex container mx-auto gap-8">
       <div className="w-2/3 flex flex-col mt-8">
-        <div className="mb-4">
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search among posts or titles below..."
-            className="my-4 block w-full px-4 py-4 bg-white text-black border-2 border-heading rounded-lg text-xl placeholder-black focus:outline-none focus:border-heading focus:ring-1 focus:ring-heading invalid:border-warning invalid:text-warning focus:invalid:border-warning focus:invalid:ring-warning "
-          />
-        </div>
-        {filteredPosts.length > 0 ? (
+        {user && (
+          <div className="mb-4">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="Search among posts or titles below..."
+              className="my-4 block w-full px-4 py-4 bg-white text-black border-2 border-heading rounded-lg text-xl placeholder-black focus:outline-none focus:border-heading focus:ring-1 focus:ring-heading invalid:border-warning invalid:text-warning focus:invalid:border-warning focus:invalid:ring-warning "
+            />
+          </div>
+        )}
+
+        {filteredPosts.length > 0 && user ? (
           filteredPosts?.map((post) => (
             <span key={post?.id} className="mb-8">
               <SinglePostCard post={post} />
